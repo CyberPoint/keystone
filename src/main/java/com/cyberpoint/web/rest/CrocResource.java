9e046a18700c627166f8fce72c31bb3708afb9b6
Aidan <aidanshawyer7@gmail.com>
2023-06-29 09:52:19 -0700

    Fixed controller, 200 response

--- src/main/java/com/cyberpoint/web/rest/CrocResource.java
+++ src/main/java/com/cyberpoint/web/rest/CrocResource.java
@@ -15,6 +15,12 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cyberpoint.domain.Task;
 import com.cyberpoint.repository.TaskRepository;
+import com.cyberpoint.service.AgentService;
+import com.cyberpoint.service.CallBackService;
+import com.cyberpoint.service.PlatformService;
+import com.cyberpoint.service.RegistrationEventService;
+import com.cyberpoint.service.RegistrationSecretService;
+import com.cyberpoint.service.TaskResultService;
 import com.cyberpoint.service.TaskService;
 
 import tech.jhipster.web.util.PaginationUtil;
@@ -28,9 +34,27 @@ public class CrocResource {
 
     private final Logger log = LoggerFactory.getLogger(CrocResource.class);
     
+ 
+    private TaskResultService taskResultService;
+    
     private TaskService taskService;
 
     private TaskRepository taskRepository;
+
+
+	private CallBackService callBackService;
+
+
+	private AgentService agentService;
+
+
+	private PlatformService platformService;
+
+
+	private RegistrationSecretService registrationSecretService;
+
+
+	private RegistrationEventService registrationEventService;
     /**
      * GET defaultAction
      */
@@ -46,5 +70,25 @@ public class CrocResource {
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
         return ResponseEntity.ok().headers(headers).body(page.getContent());
     }
+    
+    public CrocResource(
+    	    CallBackService callBackService,
+    	    RegistrationSecretService registrationSecretService,
+    	    RegistrationEventService registrationEventService,
+    	    TaskService taskService,
+    	    TaskResultService taskResultService,
+    	    AgentService agentService,
+    	    PlatformService platformService) {
+    	    this.callBackService = callBackService;
+    	    this.taskService = taskService;
+    	    this.taskResultService = taskResultService;
+    	    this.agentService = agentService;
+    	    this.platformService = platformService;
+    	    this.registrationSecretService = registrationSecretService;
+    	    this.registrationEventService = registrationEventService;
+    	}
+
 
 }
+
+
